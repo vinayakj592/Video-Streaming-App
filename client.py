cccf0bc513075e6056e28770b67cbb80e12b3859
Vinayak Joshi <vinayakjoshi1930@gmail.com>
2023-09-17 16:35:45 +0530

    server client code added

---
+++ client.py
@@ -0,0 +1,41 @@
+import socket
+import os
+import cv2
+import pickle
+import threading
+import struct
+
+
+def sendImage():
+    os.system('python3 server.py')
+
+
+t1 = threading.Thread(target=sendImage)
+t1.start()
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '192.168.29.32'
+port = 9988
+client_socket.connect(('192.168.29.32', port))
+data = b""
+payload_size = struct.calcsize("Q")
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(8 * 1024)
+        if not packet: break
+        data += packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+    while len(data) < msg_size:
+        data += client_socket.recv(8 * 1024)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("Live Streaming Video Chat", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+client_socket.close()
+
---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+import cv2
+import pickle
+import struct
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host_ip = '192.168.29.32'
+print('[+] Connecting to : ', host_ip)
+port = 9988
+socket_address = (host_ip, port)
+
+server_socket.bind(socket_address)
+
+server_socket.listen(5)
+print("[*] Listening as : ", socket_address)
+
+while True:
+    client_socket, addr = server_socket.accept()
+    print('[+] Connected:', addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+        vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
+        while (vid.isOpened()):
+            img, frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a)) + a
+            client_socket.sendall(message)
+            cv2.imshow("Sender's Video", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
